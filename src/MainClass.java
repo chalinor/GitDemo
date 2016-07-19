f19b3e2a830f969c023402f5b258218684e14ad0
chalinorbaliuag <chalinorbaliuag@192.168.20.100>
2016-07-19 22:02:51 +1200

    created the two classes

---
+++ src/MainClass.java
@@ -0,0 +1,9 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	PersonInfo p1 = new PersonInfo("Jane Doe");
+	p1.displayInfo();
+	}
+
+}
---
+++ src/PersonInfo.java
@@ -0,0 +1,14 @@
+
+public class PersonInfo {
+
+	private String name;
+
+	public PersonInfo(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public void displayInfo(){
+		System.out.println("Name: "+name);
+	}
+}
