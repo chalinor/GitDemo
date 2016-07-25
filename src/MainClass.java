02c39a633fac02e403af4d49c504d1a70ce2012a
chalinor <chalinor.baliuag@weltec.ac.nz>
2016-07-26 08:28:19 +1200

    Update MainClass.java
    added a new object

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,6 +4,10 @@ public class MainClass {
 	public static void main(String[] args) {
 	PersonInfo p1 = new PersonInfo("Jane Doe");
 	p1.displayInfo();
+	
+	PersonInfo p2 = new PersonInfo("John Doe");
+	p2.displayInfo();
+	
 	}
 
 }
