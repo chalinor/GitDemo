287275823167776ee80536db5a4004a185c193c6
chalinor.baliuag <chalinor.baliuag@T7-STAFF21.soit.local>
2017-08-09 10:49:58 +1200

    added getter and setter

--- src/PersonInfo.java
+++ src/PersonInfo.java
@@ -11,4 +11,15 @@ public class PersonInfo {
 	public void displayInfo(){
 		System.out.println("Name: "+name);
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
+
